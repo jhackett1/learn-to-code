6255ef2168ab7ddfb5b2938e82a867e8c7376bd8
jhackett1 <joshua.hackett93@gmail.com>
2018-05-16 11:05:16 +0100

    Plenary quiz check answer functionality

--- pages/roadmap.md
+++ pages/roadmap.md
@@ -4,4 +4,22 @@ order: 2
 on_menu: true
 ---
 
-Roadmap will go here
+<p class="lede">We are always improving the DDaT Codelabs curriculum and teaching methods. Here's what to expect.</p>
+
+## Spring 2018
+
+- User research project in alpha phase
+
+## Summer 2018
+
+- Supplement lessons with visuals and video content
+
+## Autumn 2018
+
+- Enters public beta
+- First cohort of DDaT fast streamers begin working on curriculum
+
+## Winter 2018/19
+
+- Subject matter specialists across government will have refined course content
+- Start to incorporate findings from first cohort
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -2,21 +2,50 @@ import React from 'react'
 import Link from 'gatsby-link'
 import slugify from 'slugify'
 
-export  default ({ question, answers }) => (
-  <form className="plenary-quiz">
-    <div className="form-group">
-      <fieldset>
-        <legend>
-          <h3 className="heading-medium">{question}</h3>
-        </legend>
-        {answers.map((answer, i) =>(
-          <div key={i} className="multiple-choice">
-            <input id={`radio-${i}`} type="radio" name="radio-group" value={answer.answer}/>
-            <label htmlFor={`radio-${i}`}>{answer.answer}</label>
-          </div>
-        ))}
-      </fieldset>
-    </div>
-    <button className="button" type="submit">Check answer</button>
-  </form>
-)
+export default class Quiz extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      checked: false,
+      selectedAnswer: ""
+    }
+  }
+
+  render(){
+    let question = this.props.question
+    let answers = this.props.answers
+
+    let handleAnswerSelect = (e) => {
+      this.setState({
+        selectedAnswer: e.target.value
+      })
+    }
+
+    let checkAnswer = (e) => {
+      e.preventDefault()
+      if (this.state.selectedAnswer !== "") {
+        this.setState({checked: true})
+      }
+    }
+
+    return(
+      <form className="plenary-quiz">
+        <div className="form-group">
+          <fieldset>
+            <legend>
+              <h3 className="heading-medium">{question}</h3>
+            </legend>
+            {answers.map((answer, i) =>(
+              <div key={i} className="multiple-choice">
+                <input id={`radio-${i}`} type="radio" name="radio-group" value={answer.answer} checked={this.state.selectedAnswer === answer.answer} onChange={handleAnswerSelect} disabled={this.state.checked} />
+                <label htmlFor={`radio-${i}`}>{answer.answer}</label>
+                {(answer.correct) ? <span className={(this.state.checked ? "answer-icon correct visible" : "answer-icon correct")}>&#10004;</span> : <span className={(this.state.checked ? "answer-icon incorrect visible" : "answer-icon incorrect")}>&#10006;</span> }
+              </div>
+            ))}
+          </fieldset>
+        </div>
+        <button className={(this.state.checked ? "button disabled" : "button")} type="submit" onClick={checkAnswer}>Check answer</button>
+      </form>
+    )
+  }
+}
