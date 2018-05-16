955d50c0b340c155de092184af7cfae96f852bf0
jhackett1 <joshua.hackett93@gmail.com>
2018-05-16 11:31:56 +0100

    Added minimap to sidebar

--- src/templates/lessonTemplate.js
+++ src/templates/lessonTemplate.js
@@ -23,14 +23,26 @@ export default ({ data }) => {
 
       <div className="grid-row lesson-content">
         <div className="column-two-thirds">
-          <h1 className="heading-xlarge">{data.markdownRemark.frontmatter.order}. {data.markdownRemark.frontmatter.title}</h1>
+          <h1 className="heading-xlarge">{data.markdownRemark.frontmatter.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
           {(data.markdownRemark.frontmatter.plenary_question) ?
             <Quiz question={data.markdownRemark.frontmatter.plenary_question} answers={data.markdownRemark.frontmatter.plenary_answers}/>
           : ""}
           <LessonNav allLessons={data.lessons} thisLesson={data.markdownRemark}/>
         </div>
-        <div className="column-one-third">
+        <div className="column-one-third mobile-hide">
+          <h5 className="minimap">Part of {data.markdownRemark.frontmatter.module}</h5>
+          <ol className="minimap-list">
+
+            {data.lessons.edges.filter(lesson => {
+              if (lesson.node.frontmatter.module === data.markdownRemark.frontmatter.module) {
+                return lesson
+              }
+            }).map((lesson, i) => (
+              <li key={i} className={(lesson.node.frontmatter.title === data.markdownRemark.frontmatter.title) ? "current" : ""}>{lesson.node.frontmatter.title}</li>
+            ))}
+
+          </ol>
         </div>
       </div>
     </div>
